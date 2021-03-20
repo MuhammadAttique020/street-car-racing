662ad43c54960ec1a9315d9b29a6dd6d9da4305e
msiddique-bse023 <80532472+msiddique-bse023@users.noreply.github.com>
2021-03-20 22:55:06 +0500

    Add files via upload

---
+++ ObjectMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+
+    float a, b;
+
+
+    void Update()
+    {
+        a = Input.GetAxis("Horizontal");
+        transform.Translate(Input.GetAxis("Horizontal") * 15f * Time.deltaTime, 0f, 0f);
+        Vector3 character = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            character.x = 10;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            character.y = 10;
+        }
+        transform.localScale = character;
+
+        b = Input.GetAxis("Vertical");
+        transform.Translate(0f, Input.GetAxis("Vertical") * 15f * Time.deltaTime, 0f);
+        Vector3 character2 = transform.localScale;
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            character2.x = 10;
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            character2.y = 10;
+        }
+        transform.localScale = character2;
+    }
+
+}
